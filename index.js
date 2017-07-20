5862a9947c4ca1536eec156f5366853edd194ac5
Bethany Skurrie <bethesk@gmail.com>
2017-07-20 15:48:35 +1000

    Some improvements

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const glob = require('glob');
 const path = require('path');
 const resemble = require('node-resemble');
 const moment = require('moment');
+const exif = require('fast-exif');
 
 function getDateFromFilePath(imagePath) {
   let dasherizedImagePath = imagePath.replace(/_/g, '-');
@@ -36,19 +37,21 @@ function compareImages(targetImagePath, testImagePath, handleMatchFound){
   });
 }
 
-function prioritiseImagesWithSimilarDates(targetImagePath, testImagePaths) {
+function prioritiseImagesWithSimilarDates(targetImagePath, testImages) {
   let targetImageDate = getDateFromFilePath(targetImagePath);
   let testImagePathsWithDateDiffs = [];
 
-  testImagePaths.forEach(function(testImagePath, index){
-    let dateDifference = targetImageDate.diff(getDateFromFilePath(testImagePath));
-    testImagePathsWithDateDiffs.push({path: testImagePath, diff: Math.abs(dateDifference)});
+  testImages.forEach(function(testImage, index){
+    let dateDifference = targetImageDate.diff(testImage.dateTaken);
+    testImagePathsWithDateDiffs.push({path: testImage.path, diff: Math.abs(dateDifference)});
   });
 
   let sorted = testImagePathsWithDateDiffs.sort(function(a, b){
     return a.diff - b.diff;
   });
-  return sorted.map(obj => { return obj.path });
+  let prioritisedImagePaths = sorted.map(obj => { return obj.path });
+  //console.log("target date", targetImageDate, "first check", prioritisedImagePaths[0], "last check", prioritisedImagePaths[prioritisedImagePaths.length - 1]);
+  return prioritisedImagePaths;
 }
 
 function createDir(dir) {
@@ -69,14 +72,8 @@ function copyTestImageToSearchResultsDir(testImagePath, searchResultsDir){
   copyFile(testImagePath, testImageDestination);
 }
 
-function findOriginalImages(targetImagesDir, searchDirs, searchResultsDir) {
-  let targetImagePaths = glob.sync(targetImagesDir + '/*.{jpg,jpeg}', {});
-  let foundTargetImageDir = path.join(targetImagesDir, 'found');
-  let searchResults = {};
-  createDir(foundTargetImageDir);
-  createDir(searchResultsDir);
-
-  var handleMatchFound = function(targetImagePath, testImagePath) {
+function createMatchFoundHandler(searchResults, foundTargetImageDir, searchResultsDir){
+  return function(targetImagePath, testImagePath) {
     if(!searchResults[targetImagePath]) {
       console.log("MATCHED!", targetImagePath, "with", testImagePath);
       searchResults[targetImagePath] = testImagePath;
@@ -84,29 +81,81 @@ function findOriginalImages(targetImagesDir, searchDirs, searchResultsDir) {
       copyTestImageToSearchResultsDir(testImagePath, searchResultsDir);
     }
   };
+}
 
+function getTestImagePaths(searchDirs) {
   let testImagePaths = [];
   searchDirs.forEach(searchDir => {
     testImagePaths = testImagePaths.concat(glob.sync(searchDir + "/*.{jpg,jpeg}", {}));
   });
+  return testImagePaths;
+}
 
+function doSearch(targetImagePaths, testImages, searchResults, handleMatchFound) {
   targetImagePaths.forEach(function(targetImagePath){
-    let prioritisedImagePaths = prioritiseImagesWithSimilarDates(targetImagePath, testImagePaths);
+    let prioritisedImagePaths = prioritiseImagesWithSimilarDates(targetImagePath, testImages);
     prioritisedImagePaths.forEach(testImagePath => {
       if(!searchResults[targetImagePath]) {
         compareImages(targetImagePath, testImagePath, handleMatchFound);
       }
     });
   });
+}
+
+function findOriginalImages(targetImagesDir, searchDirs, searchResultsDir) {
+  let searchResults = {};
+  let foundTargetImageDir = path.join(targetImagesDir, 'found');
+  createDir(foundTargetImageDir);
+  createDir(searchResultsDir);
+
+  let targetImagePaths = glob.sync(targetImagesDir + '/*.{jpg,jpeg}', {});
+  let testImagePaths = getTestImagePaths(searchDirs);
+  let handleMatchFound = createMatchFoundHandler(searchResults, foundTargetImageDir, searchResultsDir);
+
+  let buildImagesPromise = buildImages(testImagePaths);
+
+  buildImagesPromise.then(testImages => {
+    doSearch(targetImagePaths, testImages, searchResults, handleMatchFound);
+    console.log(searchResults);
+  }).catch(reason => {
+    console.log(reason);
+  })
 
   return searchResults;
 }
 
-let targetImagesDir = '/Users/Beth/Pictures/Tinybeans/2016-photos';
-let searchDirs = glob.sync("/Users/Beth/Pictures/_EXPORTS/*", {});
-// let searchDirs = ['/Users/Beth/Dropbox/Camera Uploads 2017-06-30'];
+function buildImage(path) {
+  return new Promise(function(resolve, reject) {
+    function success(exifData) {
+      let dateTaken = exifData.exif.DateTimeOriginal;
+      resolve({path: path, dateTaken: moment(dateTaken)});
+    }
+    function error(error) {
+      console.log(error);
+      resolve({path: path, dateTaken: moment()});
+    }
+    return exif.read(path).then(success).error(error);
+  });
+}
+
+function buildImages(testImagePaths) {
+  let promises = testImagePaths.map(path => {
+    return buildImage(path);
+  });
+
+  return new Promise(function(resolve, reject) {
+    Promise.all(promises)
+      .then(results => { resolve(results) })
+      .catch(reason => { console.log(reason) });
+  });
+}
+
+
+// let targetImagesDir = '/Users/Beth/Pictures/Tinybeans/2016-photos';
+// let searchDirs = glob.sync("/Users/Beth/Pictures/_EXPORTS/*", {});
+//let searchDirs = ['/Users/Beth/Dropbox/Camera Uploads 2017-06-30'];
+let targetImagesDir = 'target-images'
+let searchDirs = ['test-images-2'];
 let searchResultsDir = 'search-results';
 console.log("Searching for originals for ", targetImagesDir, "in", searchDirs);
 let searchResults = findOriginalImages(targetImagesDir, searchDirs, searchResultsDir);
-
-console.log(searchResults);
